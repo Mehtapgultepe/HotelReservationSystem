63739336a037cf4565013702a8a0ec46a442884e
mehtapgultepe <mehtapgultepe@localhost>
2025-12-30 12:55:32 +0300

    update Customer class

--- src/hotel/Customer.java
+++ src/hotel/Customer.java
@@ -2,16 +2,35 @@ package hotel;
 public class Customer {
     private String name;
     private String number;
+    private int id;
 
     public Customer(String name, String number) {
-        this.name = name;
+        this.setName(name);
         this.setNumber(number);
     }
-    public String getName() { return name; }
+
 	public String getNumber() {
 		return number;
 	}
+
 	public void setNumber(String number) {
 		this.number = number;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 }
