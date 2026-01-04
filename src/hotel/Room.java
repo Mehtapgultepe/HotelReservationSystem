4d42a5ec8c246cc510d271a9e5b5eefc6fa86eca
mehtapgultepe <mehtapgultepe@localhost>
2026-01-04 22:47:32 +0300

    update Room Class

--- src/hotel/Room.java
+++ src/hotel/Room.java
@@ -1,5 +1,51 @@
 package hotel;
 
-public class Room {
 
+public abstract class Room implements Reservable {
+    private int roomNumber;
+    private double basePrice;
+    private boolean isReserved;
+    private Customer currentCustomer;
+
+    public Room(int roomNumber, double basePrice) {
+        this.roomNumber = roomNumber;
+        this.basePrice = basePrice;
+        this.isReserved = false;
+    }
+
+    public abstract double calculatePrice(int days);
+
+    
+    @Override
+    public boolean isAvailable() {
+        return !isReserved;
+    }
+
+    @Override
+    public void reserve(Customer customer) {
+        if (!isReserved) {
+            this.isReserved = true;
+            this.currentCustomer = customer;
+            System.out.println("Room " + roomNumber + " reserved for " + customer.getName());
+        } else {
+            System.out.println("Room " + roomNumber + " is already full!");
+        }
+    }
+
+    @Override
+    public void cancel() {
+        this.isReserved = false;
+        this.currentCustomer = null;
+        System.out.println("Room " + roomNumber + " is now free.");
+    }
+
+    public int getRoomNumber() { return roomNumber; }
+    public double getBasePrice() { return basePrice; }
+    
+
+    @Override
+    public String toString() {
+        String status = isReserved ? "Full (Customer: " + currentCustomer.getName() + ")" : "Empty";
+        return "Room #" + roomNumber + " [" + status + "]"; 
+    }
 }
